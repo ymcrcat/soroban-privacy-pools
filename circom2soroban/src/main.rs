debaaabb19a217b9b161577e3d0875adeedc5c00
Yan Michalevsky <ymcrcat@gmail.com>
2025-06-24 16:25:06 -0400

    circom2soroban outputs strings

--- circom2soroban/src/main.rs
+++ circom2soroban/src/main.rs
@@ -1,12 +1,10 @@
-use ark_bls12_381::{G1Affine, G2Affine, Fq, Fq2};
-use ark_serialize::CanonicalSerialize;
-use core::str::FromStr;
 use serde::Deserialize;
 use std::fs;
 use clap::Parser;
 
 #[derive(Parser)]
 struct Args {
+    filetype: String,
     filename: String,
 }
 
@@ -20,59 +18,70 @@ struct VerificationKeyJson {
     ic: Vec<[String; 3]>,
 }
 
-fn g1_bytes(x: &str, y: &str) -> [u8; 48] {
-    let p = G1Affine::new(Fq::from_str(x).unwrap(), Fq::from_str(y).unwrap());
-    let mut buf = [0u8; 48];
-    p.serialize_compressed(&mut buf[..]).unwrap();
-    buf
+#[derive(Deserialize)]
+struct ProofJson {
+    pi_a: [String; 3],
+    pi_b: [[String; 2]; 3],
+    pi_c: [String; 3],
+    #[serde(rename = "protocol")]
+    _protocol: String,
+    #[serde(rename = "curve")]
+    _curve: String
 }
 
-fn g2_bytes(x1: &str, x2: &str, y1: &str, y2: &str) -> [u8; 96] {
-    let x = Fq2::new(Fq::from_str(x1).unwrap(), Fq::from_str(x2).unwrap());
-    let y = Fq2::new(Fq::from_str(y1).unwrap(), Fq::from_str(y2).unwrap());
-    let p = G2Affine::new(x, y);
-    let mut buf = [0u8; 96];
-    p.serialize_compressed(&mut buf[..]).unwrap();
-    buf
+fn print_vk(json_str: &String)
+{
+    let vk: VerificationKeyJson = serde_json::from_str(json_str).expect("Invalid JSON");
+
+    println!("let alphax = \"{}\";", vk.vk_alpha_1[0]);
+    println!("let alphay = \"{}\";", vk.vk_alpha_1[1]);
+    println!("\n");
+    println!("let betax1 = \"{}\";", vk.vk_beta_2[0][0]);
+    println!("let betax2 = \"{}\";", vk.vk_beta_2[0][1]);
+    println!("let betay1 = \"{}\";", vk.vk_beta_2[1][0]);
+    println!("let betay2 = \"{}\";", vk.vk_beta_2[1][1]);
+    println!("\n");
+    println!("let gammax1 = \"{}\";", vk.vk_gamma_2[0][0]);
+    println!("let gammax2 = \"{}\";", vk.vk_gamma_2[0][1]);
+    println!("let gammay1 = \"{}\";", vk.vk_gamma_2[1][0]);
+    println!("let gammay2 = \"{}\";", vk.vk_gamma_2[1][1]);
+    println!("\n");
+    println!("let deltax1 = \"{}\";", vk.vk_delta_2[0][0]);
+    println!("let deltax2 = \"{}\";", vk.vk_delta_2[0][1]);
+    println!("let deltay1 = \"{}\";", vk.vk_delta_2[1][0]);
+    println!("let deltay2 = \"{}\";", vk.vk_delta_2[1][1]);
+    println!("\n");
+    println!("let ic0x = \"{}\";", vk.ic[0][0]);
+    println!("let ic0y = \"{}\";", vk.ic[0][1]);
+    println!("\n");
+    println!("let ic1x = \"{}\";", vk.ic[1][0]);
+    println!("let ic1y = \"{}\";", vk.ic[1][1]);
 }
 
-fn print_bytes(bytes: &[u8]) -> String {
-    bytes.iter().map(|b| format!("0x{:02x}", b)).collect::<Vec<_>>().join(", ")
+fn print_proof(json_str: &String) {
+    let proof: ProofJson = serde_json::from_str(json_str).expect("Invalid JSON");
+
+    println!("let pi_ax = \"{}\";", proof.pi_a[0]);
+    println!("let pi_ay = \"{}\";", proof.pi_a[1]);
+    println!("\n");
+    println!("let pi_bx1 = \"{}\";", proof.pi_b[0][0]);
+    println!("let pi_bx2 = \"{}\";", proof.pi_b[0][1]);
+    println!("let pi_by1 = \"{}\";", proof.pi_b[1][0]);
+    println!("let pi_by2 = \"{}\";", proof.pi_b[1][1]);
+    println!("\n");
+    println!("let pi_cx = \"{}\";", proof.pi_c[0]);
+    println!("let pi_cy = \"{}\";", proof.pi_c[1]);
 }
 
 fn main() {
     let args = Args::parse();
     let json_str = fs::read_to_string(&args.filename).expect("Failed to read file");
-    let vk: VerificationKeyJson = serde_json::from_str(&json_str).expect("Invalid JSON");
-
-    // Alpha
-    let alpha_bytes = g1_bytes(&vk.vk_alpha_1[0], &vk.vk_alpha_1[1]);
-    // Beta
-    let beta_bytes = g2_bytes(
-        &vk.vk_beta_2[0][0], &vk.vk_beta_2[0][1],
-        &vk.vk_beta_2[1][0], &vk.vk_beta_2[1][1]
-    );
-    // Gamma
-    let gamma_bytes = g2_bytes(
-        &vk.vk_gamma_2[0][0], &vk.vk_gamma_2[0][1],
-        &vk.vk_gamma_2[1][0], &vk.vk_gamma_2[1][1]
-    );
-    // Delta
-    let delta_bytes = g2_bytes(
-        &vk.vk_delta_2[0][0], &vk.vk_delta_2[0][1],
-        &vk.vk_delta_2[1][0], &vk.vk_delta_2[1][1]
-    );
-
-    println!("VerificationKey {{");
-    println!("    alpha: G1Affine::from_compressed(&env, &BytesN::from_array(&env, [{}])),", print_bytes(&alpha_bytes));
-    println!("    beta: G2Affine::from_compressed(&env, &BytesN::from_array(&env, [{}])),", print_bytes(&beta_bytes));
-    println!("    gamma: G2Affine::from_compressed(&env, &BytesN::from_array(&env, [{}])),", print_bytes(&gamma_bytes));
-    println!("    delta: G2Affine::from_compressed(&env, &BytesN::from_array(&env, [{}])),", print_bytes(&delta_bytes));
-    println!("    ic: Vec::from_array(&env, [");
-    for ic in &vk.ic {
-        let ic_bytes = g1_bytes(&ic[0], &ic[1]);
-        println!("        G1Affine::from_compressed(&env, &BytesN::from_array(&env, [{}])),", print_bytes(&ic_bytes));
+    
+    if args.filetype == "vk" {
+        print_vk(&json_str);
     }
-    println!("    ]),");
-    println!("}}");
+
+    if args.filetype == "proof" {
+        print_proof(&json_str);
+    }
 }
